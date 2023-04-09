dd7f972f0348b5193023482b301a5712814a2597
Illia Bidiuk <Core778@gmail.com>
2023-04-09 22:25:48 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -14,11 +14,10 @@ const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: 250,
     scrollZoom: false,
-  }); 
+});
 
-  const handleSearch = async event => {
+const handleSearch = async event => {
     event.preventDefault();
-
 
     const searchQuery = event.currentTarget.elements['searchQuery'].value;
     photoApi.q = searchQuery;
@@ -29,13 +28,24 @@ const lightbox = new SimpleLightbox('.gallery a', {
         if (!data.hits.length) {
             Notify.failure("Sorry, there are no images matching your search query. Please try again.");
             return;
-        }else{
+        } else {
             Notify.info(`Hooray! We found ${data.totalHits} images.`)
         }
-    
+
         galleryListEl.innerHTML = createPostsCard(data.hits);
         lightbox.refresh()
         btnLoadMoreEl.classList.remove("is-hidden");
+
+        const galleryEl = document.querySelector(".gallery");
+        const { height: cardHeight } = galleryEl.getBoundingClientRect();
+
+        if (cardHeight) {
+            window.scrollBy({
+                top: cardHeight * 2,
+                behavior: "smooth",
+            });
+        }
+
     } catch (err) {
         console.log(err);
     }
@@ -48,7 +58,7 @@ const handleLoadMore = async () => {
         const { data } = await photoApi.fetchPosts();
         galleryListEl.insertAdjacentHTML('beforeend', createPostsCard(data.hits));
         lightbox.refresh()
-        if ( photoApi.page > Math.ceil(data.totalHits / 4)) {
+        if (photoApi.page > Math.ceil(data.totalHits / 4)) {
             btnLoadMoreEl.classList.add("is-hidden");
             Notify.info("We're sorry, but you've reached the end of search results.");
         }
